2b474e339187557252185a13707bd1785b7b0ccf
Denis Bazhenov <dotsid@gmail.com>
2025-12-02 19:14:17 +0100

    Separating safe-cargo and cargo arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use safe_cargo::prepare_profile;
 use std::{
-    collections::VecDeque,
     env, fs, io,
     process::{Command, ExitCode},
 };
@@ -8,9 +7,9 @@ use std::{
 /// This crate is available only on macOS becuase it relies on `sandbox-exec` cli tool
 #[cfg(target_os = "macos")]
 fn main() -> Result<ExitCode, io::Error> {
-    let mut args = env::args().collect::<VecDeque<_>>();
-
-    let _program_name = args.pop_front();
+    // skipping program name
+    let args = env::args().skip(1).collect::<Vec<_>>();
+    let Args { cargo_args, args } = split_cargo_args(args);
 
     let Ok(workspace_path) = env::current_dir() else {
         panic!("Error reading current directory");
@@ -33,7 +32,7 @@ fn main() -> Result<ExitCode, io::Error> {
         .arg("-f")
         .arg(profile_path)
         .arg("cargo")
-        .args(args)
+        .args(cargo_args)
         .env("CARGO_TARGET_DIR", sandbox_path.join("target"))
         .env("CARGO_HOME", sandbox_path.join("cargo"))
         .spawn()?
@@ -44,4 +43,54 @@ fn main() -> Result<ExitCode, io::Error> {
         _ => ExitCode::FAILURE,
     };
     Ok(code)
+}
+
+struct Args<T: AsRef<str>> {
+    /// arguments of `safe-cargo`
+    args: Vec<T>,
+    /// arguments passed to a cargo command
+    cargo_args: Vec<T>,
+}
+
+fn split_cargo_args<T: AsRef<str>>(mut args: Vec<T>) -> Args<T> {
+    let double_dash_idx = args
+        .iter()
+        .enumerate()
+        .find(|(_, i)| i.as_ref() == "--")
+        .map(|(idx, _)| idx);
+    if let Some(double_dash_idx) = double_dash_idx {
+        let cargo_args = args.split_off(double_dash_idx + 1);
+        // Removing double dash from the list of our arguments
+        args.pop();
+        Args { cargo_args, args }
+    } else {
+        Args {
+            cargo_args: args,
+            args: vec![],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_split_cargo_args() {
+        macro_rules! assert_split {
+            ([$($args:expr),*] => [$($expected_args:expr),*], [$($expected_cargo_args:expr),*]) => {
+                let Args { args, cargo_args } = split_cargo_args::<&str>(vec![$($args),*]);
+                assert_eq!(args, &[$($expected_args),*] as &[&str], "safe-cargo arguments are not valid");
+                assert_eq!(cargo_args, &[$($expected_cargo_args),*] as &[&str], "cargo arguments are not valid");
+            };
+        }
+
+        assert_split!([] => [], []);
+        assert_split!(["build"] => [], ["build"]);
+        assert_split!(["+nightly", "build"] => [], ["+nightly", "build"]);
+        assert_split!(["--", "+nightly", "build"] => [], ["+nightly", "build"]);
+        assert_split!(["--dump", "--", "+nightly", "build"] => ["--dump"], ["+nightly", "build"]);
+        assert_split!(["--dump", "--"] => ["--dump"], []);
+        assert_split!(["--dump", "--", "run", "--", "-a"] => ["--dump"], ["run", "--", "-a"]);
+    }
 }
